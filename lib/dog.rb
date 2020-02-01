754c488b1f0b7f34e653f1fd1f3c9e1e37253ff9
Ramesh Chuwan <chhetrirahul2015@gmail.com>
2020-02-01 19:34:46 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,2 +1,27 @@
-
+class Dog 
+  @@all = []
+  attr_accessor :name
+  def initialize(name)
+    @name = name 
+    @@all << self
+  end
+  
+  def self.all 
+    @@all 
+  end
+  
+  def self.clear_all
+    @@all.clear
+  end
+  
+  def self.print_all 
+    @@all.each do |dogs|
+      puts dogs.name
+    end
+  end
+  
+  def self.save 
+    @@all << save
+  end
+end
   
